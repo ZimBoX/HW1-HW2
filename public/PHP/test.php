547c28e1c41dc973352813a8f23884cb5dceded9
ZimBoX <SmailCat06@yandex.ru>
2023-01-16 22:35:28 +0300

    fix server problems

--- public/PHP/test.php
+++ public/PHP/test.php
@@ -5,7 +5,5 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Credentials: true');
 
-$input = file_get_contents("PHP://input");
-var_dump($_GET, $input, $_POST);
-echo $input;
+echo 123;
 ?>
--- src/router/HW1/HW1.jsx
+++ src/router/HW1/HW1.jsx
@@ -1,16 +1,27 @@
+import { useState, useEffect } from 'react';
+
 import axios from "axios";
 
 import "./HW1.css";
 
 function HW1() {
-  axios
-    .post('/php/functions.php', {
-      firstName: "Fred",
-      lastName: "Flintstone",
-    })
-    .then(function (response) {
-      console.log(response);
-    });
+
+    const [axiosURL, setAxiosURL] = useState([]);
+
+    useEffect( () => {
+        if (axiosURL.length === 0){
+            axios.get('php/functions.php')
+            .then(function (response) {
+                setAxiosURL(response.request.responseURL);
+                return;
+            })
+            .catch(function () {
+                setAxiosURL("http://hw1.box/functions.php");
+                return;
+            });
+        }
+    }, [] )
+
 }
 
 export default HW1;
